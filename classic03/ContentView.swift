473539634a9250a95d2bf00de00c58dcc5a5e08e
kunyuli <kunyuli16@gmail.com>
2021-09-10 20:35:41 +0800

    Initial Commit

---
+++ classic03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classic03
+//
+//  Created by 暨大附中4 on 2021/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classic03/classic03App.swift
@@ -0,0 +1,17 @@
+//
+//  classic03App.swift
+//  classic03
+//
+//  Created by 暨大附中4 on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct classic03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
