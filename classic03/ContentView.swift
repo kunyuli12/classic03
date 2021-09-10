6899a5c389e83478341f90c7716ace1008c543ca
kunyuli <kunyuli16@gmail.com>
2021-09-10 20:41:05 +0800

    Text內文

--- classic03/ContentView.swift
+++ classic03/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack{
         Text("Hello, world!")
-            .padding()
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)//標題
+        Text("描述內容")
+            .font(.body)//內文字體
+            
+        }
     }
 }
 
